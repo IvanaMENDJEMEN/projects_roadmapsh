137cfabbe489cdea689f97ee29f65c7682c45999
IvanaMENDJEMEN <soniayoumbi03@gmail.com>
2025-07-17 17:21:45 +0100

    Ajout du projet tabs

---
+++ tabs/interaction.js
@@ -0,0 +1,6 @@
+const home = document.getElementById('tabs-home')
+const project = document.getElementById('tabs-project')
+const article = document.getElementById('tabs-article')
+const contact = document.getElementById('tabs-contact')
+
+home.addEventListener('click',changetab)
