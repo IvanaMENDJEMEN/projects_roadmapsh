8e3cb42f16523b087945d7edb9118aaa78cc4ce1
IvanaMENDJEMEN <soniayoumbi03@gmail.com>
2025-07-23 14:14:12 +0100

    Mise a jour du projet todo list

--- TodoList/app.js
+++ TodoList/app.js
@@ -1,34 +1,73 @@
+const ul = document.getElementsByClassName('taskList')
+
+window.onload = function() { //exécute ce bloc quand la page est entièrement chargée.
+  const savedTasks = JSON.parse(localStorage.getItem("tasks")) || []; // récupère les tâches stockées (au format chaîne JSON)
+
+  savedTasks.forEach(taskText => {
+        const taskElement = createTaskElement(taskText);
+        const ul = document.getElementsByClassName('taskList')[0];
+        ul.appendChild(taskElement);    
+  });
+};
+
+function createTaskElement(taskText) {
+    const taskli = document.createElement('li');
+    const span = document.createElement('span');
+
+    taskli.textContent = taskText;
+
+    // Ajoute a l'element span un id et un contenu 
+    span.textContent = '🗑️'
+    span.id = 'deleteTask';
+    
+    // Lier la fonction de suppression (dissociée)
+    span.addEventListener('click', deleteTask);
+
+     //Ajoute le span comme un element enfant a li 
+    taskli.appendChild(span);
+
+    return taskli;
+}
 
 function addTask() {
     const input = document.getElementById('myinput');
-    const taskText = input.value;
+    const taskText = input.value.trim();
+
     if (taskText === '') {
         alert('Aucune Tache Saisie');
     }
     else {
 
-        //cree l'element li et ajoute la tache entrer par l'utilisateur comme contenu 
-        const taskli = document.createElement('li');
-        taskli.textContent = taskText;
-
-        // cree et Ajoute a l'element span un id et un contenu 
-        const span = document.createElement('span');
-        span.textContent = '🗑️'
-        span.id = 'deleteTask';
-
-        //Ajoute le span comme un element enfant a li 
-        taskli.appendChild(span);
-
-        //Ajoute l'element li comme enfant de ul 
-        const ul = document.getElementsByClassName('taskList')
-
-        //ajoute li comme le premier element de ul  (Ajuster plusieurs taches)
-        ul[0].appendChild(taskli);
-        console.log('ul = ', ul)
-
+        const taskElement = createTaskElement(taskText);
+        saveTask(taskText);
+        const ul = document.getElementsByClassName('taskList')[0];
+        ul.appendChild(taskElement);
+        
+        // reinitialise input
         input.value = '';
+        input.focus();
     }
+}
+ 
+//Supprime la tache
+function deleteTask(event) {
+    const span = event.target;
+    const li = span.parentElement;
+    li.remove();
+    const taskText = li.childNodes[0].nodeValue.trim();
+    removeTask(taskText);
+}
 
+function saveTask(task) {
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function removeTask(taskToRemove) {
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  const updatedTasks = tasks.filter(task => task !== taskToRemove);
+  localStorage.setItem("tasks", JSON.stringify(updatedTasks));
 }
 
 
